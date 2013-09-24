2deb0d66af99975b30e72c935e0cd43153c09d4d
Salvador Farfan <salfarfan@gmail.com>
2013-09-23 19:51:32 -0500

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MutulDispatch::Application.config.secret_key_base = '848faf9a1a627bf232052f0203e2bf2baad50bc32747666d00bd9b13ee1d670898d967879315565ad869ab64879c3861c9f6007941c26279e6458159b2ae2fef'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+MutulDispatch::Application.config.session_store :cookie_store, key: '_mutul_dispatch_session'
